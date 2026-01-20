7c52c66bfee64e6888555121a8d4fc9f652a54e1
NicolasTfile <nicolastfile@gmail.com>
2026-01-20 19:49:23 +0100

    worked on some integration tests for raffle contract

--- test/integration/interactions.t.sol
+++ test/integration/interactions.t.sol
@@ -11,4 +11,103 @@ pragma solidity ^0.8.19;
 // stateless fuzz
 // formal verification
 
-contract Interactions {}
+import {Test} from "forge-std/Test.sol";
+import {DeployRaffle} from "script/DeployRaffle.s.sol";
+import {Raffle} from "src/Raffle.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+
+contract Interactions is Test {
+    Raffle public raffle;
+    HelperConfig public helperConfig;
+
+    address public PLAYER = makeAddr("player");
+    uint256 public constant STARTING_PLAYER_BALANCE = 10 ether;
+
+    event RaffleEntered(address indexed player);
+
+    function setUp() public {
+        DeployRaffle deployer = new DeployRaffle();
+        (raffle, helperConfig) = deployer.deployContract();
+        vm.deal(PLAYER, STARTING_PLAYER_BALANCE);
+    }
+
+    // ENTER RAFFLE TESTS
+
+    function testPlayerCanEnterRaffle() public {
+        // Arrange
+        uint256 entranceFee = raffle.getEntranceFee();
+        // Act
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        // Assert
+        address playerRecorded = raffle.getPlayer(0);
+        assertEq(playerRecorded, PLAYER);
+    }
+
+    function testEnteringRaffleEmitsEvent() public {
+        // Arrange
+        uint256 entranceFee = raffle.getEntranceFee();
+        // Act / Assert
+        vm.expectEmit(true, false, false, false, address(raffle));
+        emit RaffleEntered(PLAYER);
+
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+    }
+
+    function testRevertsWhenNotEnoughEthSent() public {
+        // Arrange
+        uint256 insufficientFee = 0.001 ether;
+        // Act / Assert
+        vm.prank(PLAYER);
+        vm.expectRevert(Raffle.Raffle__SendMoreToEnterRaffle.selector);
+        raffle.enterRaffle{value: insufficientFee}();
+    }
+
+    function testMultiplePlayersCanEnter() public {
+        // Arrange
+        uint256 entranceFee = raffle.getEntranceFee();
+        address player2 = makeAddr("player2");
+        address player3 = makeAddr("player3");
+
+        vm.deal(player2, STARTING_PLAYER_BALANCE);
+        vm.deal(player3, STARTING_PLAYER_BALANCE);
+
+        // Act
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+
+        vm.prank(player2);
+        raffle.enterRaffle{value: entranceFee}();
+
+        vm.prank(player3);
+        raffle.enterRaffle{value: entranceFee}();
+
+        // Assert
+        assertEq(raffle.getPlayer(0), PLAYER);
+        assertEq(raffle.getPlayer(1), player2);
+        assertEq(raffle.getPlayer(2), player3);
+    }
+
+    function testCannotEnterWhenRaffleIsCalculating() public {
+        // Arrange
+        uint256 interval = raffle.getInterval();
+        uint256 entranceFee = raffle.getEntranceFee();
+
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+
+        // warp time and perform upkeep to change state to calculating
+        vm.warp(block.timestamp + interval + 1);
+        vm.roll(block.number + 1);
+        raffle.performUpkeep("");
+
+        // Act / Assert
+        address player2 = makeAddr("player2");
+        vm.deal(player2, STARTING_PLAYER_BALANCE);
+
+        vm.prank(player2);
+        vm.expectRevert(Raffle.Raffle__RaffleNotOpen.selector);
+        raffle.enterRaffle{value: entranceFee}();
+    }
+}
