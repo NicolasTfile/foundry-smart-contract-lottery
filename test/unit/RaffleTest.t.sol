28725e32a27db7836cd31c0295552823ed54a5fd
NicolasTfile <nicolastfile@gmail.com>
2026-01-25 12:12:38 +0100

    fixed console logging import issues in unit tests for raffle contract

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {DeployRaffle} from "script/DeployRaffle.s.sol";
 import {Raffle} from "src/Raffle.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
 import {Vm} from "forge-std/Vm.sol";
 import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
 import {CodeConstants} from "script/HelperConfig.s.sol";
-import {console} from "forge-std/console.sol";
 
 contract RaffleTest is CodeConstants, Test {
     Raffle public raffle;
